1c97e5a7524465169c878fe210cc0e8d21f62061
Zuly Vargas <zuly.vargas@mail.escuelaing.edu.co>
2022-08-30 19:55:56 -0500

    Refactor name package -users

--- src/test/java/edu/eci/ieti/users/UsersApplicationTests.java
+++ src/test/java/edu/eci/ieti/users/UsersApplicationTests.java
@@ -1,10 +1,10 @@
-package edu.eci.ieti.tasks;
+package edu.eci.ieti.users;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TasksApplicationTests {
+class UsersApplicationTests {
 
 	@Test
 	void contextLoads() {
